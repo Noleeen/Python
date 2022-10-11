6723ef328de76b6fdd2665812e58b8cc4bccb216
Ivan Gerasimenko <noleen@ya.ru>
2022-10-11 19:47:49 +0300

    python lection 1

---
+++ hello/hello.py
@@ -0,0 +1,61 @@
+# s = 'hello'
+# a = 555
+# b = 444
+# print (a,s,b)
+# print ('{1} - {0} - {2}'.format(a,s,b))
+# print (f'{a} - {s} - {b}')
+
+# f = True
+# h = False
+# print(f, h)
+# list = [1,   2,3,   'edew','werer']
+
+# a = float(input('a = '))
+# b = int(input('b = '))
+# c = round(a*b,5)
+# print(a,' * ',b, ' = ', c)
+
+# a = [2,2]
+# b = [1,2]
+# print(a==b)
+
+# f=[1,2,3,4]
+# odd = not f[0] % 2
+# print(odd)
+
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a > b:
+#     print(a)
+# elif a == b:
+#     print('they are equal')
+# else:
+#     print(b)
+
+
+# a=235
+# b=0
+# while a !=0:
+#     b = b * 10 + (a % 10)
+#     a //= 10
+# print(b)
+
+
+# list = [1,5,2,9]
+# for i in list:
+#     print(i/10)
+
+# for i in range(1,10,3):
+#     print(i)
+
+
+# for i in 'dgijoi':
+#     print(i)
+
+r = range(1,7)
+print(type(r))
+print(r)
+print('\n')
+print(type(list(r)))
+print(list(r))
